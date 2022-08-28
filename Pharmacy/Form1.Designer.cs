1b5c38ceaab2aec6cff68a8272de958ee9fc2f11
Isik-Ulusan <107641999+Isik-Ulusan@users.noreply.github.com>
2022-08-28 13:17:55 +0300

    Add files via upload

---
+++ Pharmacy/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Pharmacy
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Prescription = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Available = new System.Windows.Forms.ListBox();
+            this.NotAvailable = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(153, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "New Patient";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Prescription
+            // 
+            this.Prescription.FormattingEnabled = true;
+            this.Prescription.Location = new System.Drawing.Point(26, 122);
+            this.Prescription.Name = "Prescription";
+            this.Prescription.Size = new System.Drawing.Size(211, 342);
+            this.Prescription.TabIndex = 3;
+            this.Prescription.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(26, 478);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(428, 38);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Available
+            // 
+            this.Available.FormattingEnabled = true;
+            this.Available.Location = new System.Drawing.Point(243, 122);
+            this.Available.Name = "Available";
+            this.Available.Size = new System.Drawing.Size(211, 147);
+            this.Available.TabIndex = 5;
+            this.Available.Visible = false;
+            // 
+            // NotAvailable
+            // 
+            this.NotAvailable.FormattingEnabled = true;
+            this.NotAvailable.Location = new System.Drawing.Point(240, 318);
+            this.NotAvailable.Name = "NotAvailable";
+            this.NotAvailable.Size = new System.Drawing.Size(211, 147);
+            this.NotAvailable.TabIndex = 6;
+            this.NotAvailable.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(243, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Avaliable";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(240, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Unavailable";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(23, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Prescription";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(243, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Total:";
+            this.label4.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 528);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NotAvailable);
+            this.Controls.Add(this.Available);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Prescription);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Pharmacy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox Prescription;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox Available;
+        private System.Windows.Forms.ListBox NotAvailable;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Pharmacy/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Pharmacy
+{
+    public partial class Form1 : Form
+    {
+        private string[] meds =
+        {
+            "Parol", "Ibuprofen", "Synthriod", "Crestor", "Ventolin HFA", "Nexium", "Advair Diskus",
+            "Lantus Solostar", "Vyvanse", "Lyrica", "Spiriva", "Januvia", "Humira", "Abilify",
+            "Sovaldi", "Enbrel", "Harvoni", "Keytruda", "Remicade", "Revlimid"
+        };
+        private int[] prices = { 20, 22, 25, 36, 23, 49, 85, 76, 29, 34, 85, 68, 72, 34, 95, 15, 23, 51, 23, 67 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult msg = MessageBox.Show("Do you have a prescription", "New Patient", MessageBoxButtons.YesNo);
+            if (msg == DialogResult.Yes)
+            {
+                button1.Enabled = false;
+                label1.Visible = true;
+                label2.Visible = true;
+                label3.Visible = true;
+                label4.Visible = true;
+                button2.Visible = true;
+                Prescription.Visible = true;
+                NotAvailable.Visible = true;
+                Available.Visible = true;
+
+            }
+            else if (msg == DialogResult.No)
+            {
+                MessageBox.Show("Please get a prescription in order to buy drugs", "Prescription Needed");
+            }
+            button2.Text = "Add";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<Int32> avaPrc = new List<Int32>();
+
+            switch (button2.Text)
+            {
+                case "Add":
+                    StringReader rdr = new StringReader(Properties.Resources.Prescription);
+                    string str;
+                    while ((str = rdr.ReadLine()) != null)
+                    {
+                        Prescription.Items.Add(str);
+                    }
+                    button2.Text = "Check";
+                    break;
+
+                case "Check":
+                    bool notavailable = true;
+                    for (int i = 0; i < Prescription.Items.Count; i++)
+                    {
+                        int j = 0;
+                        while (j < meds.Length && notavailable == true)
+                        {
+                            if (meds[j] == (string) Prescription.Items[i])
+                            {
+                                notavailable = false;
+                            }
+                            j++;
+
+                        }
+
+                        if (notavailable == false)
+                        {
+                            Available.Items.Add(Prescription.Items[i]);
+                            avaPrc.Add(prices[j-1]);
+                        }
+                        else
+                        {
+                            NotAvailable.Items.Add(Prescription.Items[i]);
+                        }
+                        notavailable = true;
+                    }
+                    label4.Text = "Total: " + avaPrc.Sum() + " USD";
+                    button2.Text = "Ask for unavailable";
+                    break;
+
+                case "Ask for unavailable":
+                    DialogResult msg2 = MessageBox.Show("Do you want to get them sent from another pharmacy?", "Unavailable Medicine", MessageBoxButtons.YesNo);
+                    if (msg2 == DialogResult.Yes)
+                    {
+                        MessageBox.Show("The unavailable ones have been ordered", "Order from Another Pharmacy");
+                    }
+                    else if (msg2 == DialogResult.No)
+                    {
+                        MessageBox.Show("Order Declined", "Order from Another Pharmacy");
+                    }
+                    button2.Text = "Order Available";
+                    break;
+                case "Order Available":
+                    MessageBox.Show("Order Complete", "Checkout");
+                    button1.Enabled = true;
+                    label1.Visible = false;
+                    label2.Visible = false;
+                    label3.Visible = false;
+                    label4.Visible = false;
+                    button2.Visible = false;
+                    Prescription.Visible = false;
+                    NotAvailable.Visible = false;
+                    Available.Visible = false;
+
+                    Available.Items.Clear();
+                    NotAvailable.Items.Clear();
+                    Prescription.Items.Clear();
+
+                    button2.Text = "Add";
+                    label4.Text = "Total:";
+                    break;
+            }
+        }
+    }
+}
